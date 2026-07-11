356f12b878c1f4b36f97e8f14195a815ed0580a1
Jaylord-tech <oyediran904@gmail.com>
2026-07-11 08:04:49 +0100

    Update GitHub Pages docs build

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Princess/'
+  base: '/Princess/',
+  build: {
+    outDir: 'docs',
+    emptyOutDir: true
+  }
 });
