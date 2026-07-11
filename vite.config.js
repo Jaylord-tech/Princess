63911fcf7f9b5f4f79a88bae0c119ac3ab0bf976
Jaylord-tech <oyediran904@gmail.com>
2026-07-11 07:56:13 +0100

    Fix GitHub Pages build output

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Princess/',
-  build: {
-    outDir: 'docs',
-    emptyOutDir: true,
-  },
+  base: '/Princess/'
 });
